75518535bf965f94f273a15bbbdca4173050ad48
Tyr Mactire <tyr@pettingzoo.co>
2017-01-24 17:36:28 -0800

    Scan and Scan Result Models

---
+++ rover_a/models.py
@@ -0,0 +1,34 @@
+import datetime
+from sqlalchemy import create_engine, Column, BigInteger, DateTime, ForeignKey, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Scan(Base):
+    __tablename__ = 'scans'
+    id = Column(Integer, Sequence('scans_id_seq'), primary_key=True)
+    timestamp = Column(DateTime, onupdate=datetime.datetime.now)
+    x = Column(BigInteger)
+    y = Column(BigInteger)
+    rotation = Column(Integer)
+
+    def __repr__(self):
+        return "<Scan(id='%s', timestamp='%s', x='%s', y='%s', rotation='%s')>" % (
+            self.id, self.timestamp, self.x, self.y, self.rotation)
+
+
+class ScanResult(Base):
+    __tablename__ = 'scan_results'
+    id = Column(BigInteger, Sequence('scan_results_id_seq'), primary_key=True)
+    angle = Column(Integer)
+    distance = Column(Integer)
+    scans = Column(Integer)
+    scan_id = Column(Integer, ForeignKey('scans.id'))
+
+    scan = relationship("Scan", back_populates="scan_results")
+
+    def __repr__(self):
+        return "<ScanResult(id='%s', angle='%s', distance='%s', scans='%s' scan_id='%s')>" % (
+            self.id, self.angle, self.distance, self.scans, self.scan_id)
