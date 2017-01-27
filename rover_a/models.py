fc91cf9820141af542815ac69b7e1f670a9f1165
Tyr Mactire <tyr@pettingzoo.co>
2017-01-26 22:46:29 -0800

    fixes

--- rover_a/models.py
+++ rover_a/models.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 import datetime
-from sqlalchemy import create_engine, Column, BigInteger, DateTime, ForeignKey, Integer, String, Sequence
+from sqlalchemy import Column, BigInteger, DateTime, ForeignKey, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -9,10 +10,12 @@ Base = declarative_base()
 class Scan(Base):
     __tablename__ = 'scans'
     id = Column(Integer, Sequence('scans_id_seq'), primary_key=True)
-    timestamp = Column(DateTime, onupdate=datetime.datetime.now)
+    timestamp = Column(DateTime, default=datetime.datetime.now)
     x = Column(BigInteger)
     y = Column(BigInteger)
     rotation = Column(Integer)
+
+    scan_results = relationship("ScanResult")
 
     def __repr__(self):
         return "<Scan(id='%s', timestamp='%s', x='%s', y='%s', rotation='%s')>" % (
@@ -21,12 +24,11 @@ class Scan(Base):
 
 class ScanResult(Base):
     __tablename__ = 'scan_results'
-    id = Column(BigInteger, Sequence('scan_results_id_seq'), primary_key=True)
+    id = Column(Integer, Sequence('scan_results_id_seq'), primary_key=True)
     angle = Column(Integer)
     distance = Column(Integer)
     scans = Column(Integer)
     scan_id = Column(Integer, ForeignKey('scans.id'))
-
     scan = relationship("Scan", back_populates="scan_results")
 
     def __repr__(self):
