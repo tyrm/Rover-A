03dd1fd8045b303e81cd4d633eafe57c9c3f1cee
Wolfy Meier <wolfy@wlfy.it>
2017-04-23 14:57:50 -0700

    init db tables
    fix timestamp

--- rover_a/models.py
+++ rover_a/models.py
@@ -10,16 +10,17 @@ Base = declarative_base()
 class Scan(Base):
     __tablename__ = 'scans'
     id = Column(Integer, Sequence('scans_id_seq'), primary_key=True)
-    timestamp = Column(DateTime, default=datetime.datetime.now)
+    timestamp = Column(DateTime(timezone=True), default=datetime.datetime.now)
     x = Column(BigInteger)
     y = Column(BigInteger)
+    quality = Column(Integer)
     rotation = Column(Integer)
 
     scan_results = relationship("ScanResult")
 
     def __repr__(self):
-        return "<Scan(id='%s', timestamp='%s', x='%s', y='%s', rotation='%s')>" % (
-            self.id, self.timestamp, self.x, self.y, self.rotation)
+        return "<Scan(id='%s', timestamp='%s', x='%s', y='%s', rotation='%s', quality='%s')>" % (
+            self.id, self.timestamp, self.x, self.y, self.rotation, self.quality)
 
 
 class ScanResult(Base):
@@ -27,10 +28,9 @@ class ScanResult(Base):
     id = Column(Integer, Sequence('scan_results_id_seq'), primary_key=True)
     angle = Column(Integer)
     distance = Column(Integer)
-    scans = Column(Integer)
     scan_id = Column(Integer, ForeignKey('scans.id'))
     scan = relationship("Scan", back_populates="scan_results")
 
     def __repr__(self):
-        return "<ScanResult(id='%s', angle='%s', distance='%s', scans='%s' scan_id='%s')>" % (
-            self.id, self.angle, self.distance, self.scans, self.scan_id)
+        return "<ScanResult(id='%s', angle='%s', distance='%s' scan_id='%s')>" % (
+            self.id, self.angle, self.distance, self.scan_id)
--- rover_a/rover_a.py
+++ rover_a/rover_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import ConfigParser
 import logging
 import time
 
@@ -7,7 +8,7 @@ import Adafruit_PCA9685
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Scan, ScanResult
+from models import Base, Scan, ScanResult
 from scanner import Scanner
 from trackcontroller import TrackController
 
@@ -30,21 +31,31 @@ FORWARD_SCANNER_PULSE_MIN = 150
 FORWARD_SCANNER_PULSE_MAX = 599
 
 # Initialization
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)8s (%(threadName)-10s) %(message)s')
+#logging.basicConfig(level=logging.INFO, format='%(levelname)8s (%(threadName)-10s) %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)8s (%(threadName)-10s) %(name)s:%(message)s')
 
-logging.info("starting")
+print logging.basicConfig()
+
+logging.info("Hello! Starting a-Rex")
 
 
 class RoverA:
     def __init__(self):
-        engine = create_engine('sqlite:///brain.db', echo=True)
+        # Parse Config
+        Config = ConfigParser.ConfigParser()
+        Config.read("config.ini")
+
+        # Init DB
+        connection_string = Config.get('database', 'engine')
+        engine = create_engine(connection_string, echo=False)
+        Base.metadata.create_all(engine)
         self.Session = sessionmaker(bind=engine)
 
         self.pwm = Adafruit_PCA9685.PCA9685(I2C_ADDR_PWM_CONTROLLER)
         self.pwm.set_pwm_freq(60)
 
-        self.wheels = TrackController(GPIO_PIN_WHEEL_ENCODER_INT, mot_addr=I2C_ADDR_WHEEL_CONTROLLER,
-                                      enc_addr=I2C_ADDR_WHEEL_ENCODERS)
+        #self.wheels = TrackController(GPIO_PIN_WHEEL_ENCODER_INT, mot_addr=I2C_ADDR_WHEEL_CONTROLLER,
+        #                              enc_addr=I2C_ADDR_WHEEL_ENCODERS)
 
         self.scanner = Scanner(self.pwm, sensor_addr=I2C_ADDR_FORWARD_DISTANCE, pwm_servo_index=PWM_FORWARD_SCANNER,
                                servo_pulse_min=FORWARD_SCANNER_PULSE_MIN, servo_pulse_max=FORWARD_SCANNER_PULSE_MAX,
@@ -55,12 +66,33 @@ class RoverA:
         self.position = (0, 0)
         self.rotation = 0
 
+
     def run(self):
-        val = self.scanner.do_scan(10, 50)
+        val = self.do_scan()
+
+        session = self.Session()
+        session.add(val)
+        session.commit()
 
         print(val)
+
+        print(val.scan_results)
 
         time.sleep(5)
 
         exit()
 
+    def do_scan(self, scan_count=1, rescan_tries=5):
+        logging.info("Starting Scan Quality[{0}]".format(scan_count))
+
+        new_scan = Scan(x=self.position[0], y=self.position[1], rotation=self.rotation, quality=scan_count)
+        raw_scan = self.scanner.do_scan(scan_count=scan_count, rescan_tries=rescan_tries)
+        results = []
+
+        for deg, measurement in enumerate(raw_scan):
+            if measurement is not None:
+                results.append(ScanResult(angle=deg, distance=measurement))
+
+        new_scan.scan_results = results
+
+        return new_scan
